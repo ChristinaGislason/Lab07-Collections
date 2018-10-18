3b6e0321115d94f78d8a6409118ee221f57ecfbb
Christina Gislason <christinamkcho@gmail.com>
2018-10-17 18:13:06 -0700

    added Deck and Card classes; added enums to Card class and created custom generic collection Deck

---
+++ Lab07-Collections/Classes/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07_Collections.Classes
+{
+    class Card       
+        {
+            // the Suit is an enum
+            public CardSuit Suit { get; set; }
+            public Value Value { get; set; }
+        }
+        
+        enum CardSuit
+        {
+            Diamonds,
+            Hearts,
+            Spades,
+            Clubs
+        }
+        
+        enum Value
+        {
+            Ace = 1,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten,
+            Jack,
+            Queen,
+            King
+        }  
+    
+}
---
+++ Lab07-Collections/Classes/Deck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07_Collections.Classes
+{
+    //'IEnumerable' is an adjective; T represents a generic type
+    class Deck<T>: IEnumerable 
+    {
+        // no industry standard for size here
+        // this is the minimum size allocation
+        T[] cards = new T[5];
+        int count = 0;
+
+        public void Add(T card)
+        {
+            if (count == cards.Length)
+            {
+                // array methods are fine to use now 
+                // Resize() is not magic; it is an array under the hood, doing normal array things
+                Array.Resize(ref cards, cards.Length * 2);
+            }
+            // count++ will only increase count *after* the action has been taken. ++count would do it before
+            cards[count++] = card;
+        }
+
+        public void Remove(T card)
+        {
+           
+        }
+
+        public Deck<T> ShowSuit(string Suit)
+        {
+
+        }
+
+        //must ALWAYS do this in order to implement the IEnumberable interface 
+        public IEnumerator<T> GetEnumerator()
+        {
+            // this is the underlying loop that will allow your foreach loop to run
+            for (int i = 0; i < count; i++)
+            {
+                yield return cards[i];
+            }
+
+        }
+
+        // this is required to allow enumeration with legacy code
+        // this is not a generic, we are using it to call the version that is a generic (above)
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
